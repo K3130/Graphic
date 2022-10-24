f20fdb101dfceb35ec21673dbe1602439185216e
K3130 <a07adf2d675c@mail.ru>
2022-10-24 10:15:50 +0300

    add pyramid

--- Graphic/Graphic.hpp
+++ Graphic/Graphic.hpp
@@ -21,7 +21,7 @@ namespace Graphic
 
 			const char* glsl_version = "#version 150";
 			glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-			glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+			glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 			glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
 			glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
 			glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
