2cc1c184fbbf80632d1aef8060c0b6ec3bea560a
K3130 <a07adf2d675c@mail.ru>
2022-10-28 11:53:45 +0300

    Added GLM library

--- Graphic/common.h
+++ Graphic/common.h
@@ -3,6 +3,9 @@
 #include <imgui_stdlib.h>
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_opengl3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
